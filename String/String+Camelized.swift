ff9db5afba93663db6d14390bf7c46acc12d0d6b
WataruSuzuki <wataru0406@gmail.com>
2019-12-06 22:20:00 +0900

    Added some string extension

---
+++ String/String+Camelized.swift
@@ -0,0 +1,24 @@
+//
+//  String+Camelized.swift
+//  SwiftExtensionChimera
+//
+//  Created by WataruSuzuki on 2020/04/01.
+//  Copyright © 2019 WataruSuzuki. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    // set `lower = false` in case you want "UpperCamel" output.
+    func camelized(lower: Bool = true) -> String {
+        guard self != "" else { return self }
+
+        let words = lowercased().split(separator: "_").map({ String($0) })
+        let firstWord: String = words.first ?? ""
+
+        let camel: String = lower ? firstWord : String(firstWord.prefix(1).capitalized) + String(firstWord.suffix(from: index(after: startIndex)))
+        return words.dropFirst().reduce(into: camel, { camel, word in
+            camel.append(String(word.prefix(1).capitalized) + String(word.suffix(from: index(after: startIndex))))
+        })
+    }
+}
---
+++ String/String+Snakenized.swift
@@ -0,0 +1,37 @@
+//
+//  String+Snakenized.swift
+//  SwiftExtensionChimera
+//
+//  Created by WataruSuzuki on 2020/04/01.
+//  Copyright © 2019 WataruSuzuki. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var snakenized: String {
+        // Ensure the first letter is capital
+        let head = String(prefix(1))
+        let tail = String(suffix(count - 1))
+        let upperCased = head.uppercased() + tail
+
+        let input = upperCased
+
+        // split input string into words with Capital letter
+        let pattern = "[A-Z]+[a-z,\\d]*"
+        guard let regex = try? NSRegularExpression(pattern: pattern, options: []) else {
+            return ""
+        }
+
+        var words:[String] = []
+
+        regex.matches(in: input, options: [], range: NSRange.init(location: 0, length: count)).forEach { match in
+            if let range = Range(match.range(at: 0), in: self) {
+                words.append(String(self[range]).lowercased())
+            }
+        }
+
+        return words.joined(separator: "_")
+    }
+}
+
